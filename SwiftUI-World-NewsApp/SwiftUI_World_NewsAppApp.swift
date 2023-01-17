6a56a69668791549d5e80da4f8b0feb11a672ef6
Sebastian Worek <sebastian1.worek@gmail.com>
2023-01-17 18:09:13 +0100

    Initial Commit

---
+++ SwiftUI-World-NewsApp/SwiftUI_World_NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_World_NewsAppApp.swift
+//  SwiftUI-World-NewsApp
+//
+//  Created by Sebastian on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_World_NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
