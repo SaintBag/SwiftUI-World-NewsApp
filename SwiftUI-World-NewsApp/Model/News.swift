eef2218657cae58f61c9e1915ba3d8c141848e18
SaintBag <99802688+SaintBag@users.noreply.github.com>
2023-01-19 16:38:40 +0100

    Feature/api service (#3)
    * create URL builder
    
    * create NewsError enum, ApiServiceClass and NewsViewModel

--- SwiftUI-World-NewsApp/Model/News.swift
+++ SwiftUI-World-NewsApp/Model/News.swift
@@ -11,14 +11,37 @@ struct News: Codable {
     let articles: [Article]
 }
 
-struct Article: Codable {
+struct Article: Codable, Identifiable {
     
+    var id = UUID()
     let author: String?
     let url: String?
-    let source: String?
-    let title: String?
+    let source, title: String?
     let description: String?
     let image: String?
-    let date: Date?
+//  let date: Date()
     
+    enum CodingKeys: String, CodingKey {
+        case author
+        case url
+        case source
+        case title
+        case description
+        case image
+//      case date
+    }
+
+}
+
+extension Article {
+    
+    static var fakeNews: Article {
+        .init(author: "Kotinkova",
+              url: "https://www.cnn.com/2023/01/15/asia/nepal-yeti-airlines-crash-intl-hnk/index.html",
+              source: "CNN",
+              title: "At least 68 killed in Nepal's worst airplane crash in 30 years - CNN",
+              description: "At least 68 people were killed Sunday when an aircraft went down near the city of Pokhara in central Nepal, a government official said, the country's deadliest plane crash in more than 30 years.",
+              image: "https://media.cnn.com/api/v1/images/stellar/prod/230115050553-02-pokhara-plane-crash-011523.jpg?c=16x9&q=w_800,c_fill")
+//              date: Date())
+    }
 }
