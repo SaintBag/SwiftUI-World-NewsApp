451ec2fbfdc6bc3bba9f6da48befcebb33b13674
Sebastian Worek <sebastian1.worek@gmail.com>
2023-01-20 11:29:26 +0100

    Implementation of UrlImage for displaying article image

--- SwiftUI-World-NewsApp/NewsView.swift
+++ SwiftUI-World-NewsApp/NewsView.swift
@@ -7,6 +7,7 @@
 
 
 import SwiftUI
+import URLImage
 
 struct NewsView: View {
     
@@ -15,7 +16,20 @@ struct NewsView: View {
     var body: some View {
         
         VStack {
-            ImageView()
+            if let imageUrl = article.image,
+               let url = URL(string: imageUrl) {
+                URLImage(url, identifier: article.id.uuidString, failure: { error, _ in
+                    
+                }, content: { articlePhoto in
+                    articlePhoto
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                })
+                .frame(minWidth: 300, maxWidth: .infinity, minHeight: 250, maxHeight: 300)
+            } else {
+                ImageView()
+            }
+            
             TitleView(title: article.title)
             DescriptionView(description: article.description, source: article.source, author: article.author)
         }
@@ -30,23 +44,26 @@ struct NewsView_Previews: PreviewProvider {
 }
 
 struct ImageView: View {
+    
     var body: some View {
+        
         Image(systemName: "person")
             .resizable()
             .aspectRatio(contentMode: .fit)
             .frame(minWidth: 300, maxWidth: .infinity, minHeight: 220, maxHeight: 280)
-            .cornerRadius(20)
-        
     }
 }
 
 struct TitleView: View {
+    
     let title: String?
+    
     var body: some View {
+        
         VStack(alignment: .center) {
             Text(title ?? "")
                 .font(.title)
-                .padding(.init(top: -3, leading: 4, bottom: 20, trailing: 4))
+                .padding(.init(top: 20, leading: 4, bottom: 20, trailing: 4))
             
         }
         .padding(.horizontal, 4)
@@ -60,12 +77,12 @@ struct DescriptionView: View {
     let author: String?
     
     var body: some View {
+        
         HStack(alignment: .bottom, spacing: 4) {
             
             VStack(alignment: .trailing) {
                 
                 Text(description ?? "")
-                
                     .font(.callout)
                     .font(.system(size: 20))
                     .padding(.top, 20)
